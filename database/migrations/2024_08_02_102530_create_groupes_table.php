7706bc7a4065f502d2b198eac69db27a75457f21
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-02 12:45:44 +0200

    migration groups create

--- database/migrations/2024_08_02_102530_create_groupes_table.php
+++ database/migrations/2024_08_02_102530_create_groupes_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('groupes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('groupes');
+        Schema::dropIfExists('groups');
     }
 };
