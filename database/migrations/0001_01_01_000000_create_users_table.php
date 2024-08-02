606b0a2de74e7d1724e915d05df66bce7b35346c
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-02 15:12:53 +0200

    migration message attachemnts

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->boolean('is_admin')->default(false);
-            // $table->timestamps('blocked_at')->nullable();
+            $table->timestamp('blocked_at')->nullable();
             $table->timestamps();
         });
 
