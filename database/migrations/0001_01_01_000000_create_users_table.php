7abbba1614b49bc92315bfc60ebdcbf584ffd4f6
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-02 12:48:46 +0200

    migration conversation create

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->boolean('is_admin')->default(false);
-            $table->timestamps('blocked_at')->nullable();
+            // $table->timestamps('blocked_at')->nullable();
             $table->timestamps();
         });
 
