0d10234c046d79b7a889df19f6b71263cb037ee3
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-03 12:31:47 +0200

    changement de nom

---
+++ resources/js/Pages/Profile/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <AuthenticatedLayout>
+            <template #header>
+                <h2>Dashboard</h2>
+            </template>
+
+            <p>Vous êtes connecté</p>
+        </AuthenticatedLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+</script>
+
+<style scoped></style>
