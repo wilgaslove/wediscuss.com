a01d08d5c481bf21783a653f4d001b0079124526
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-08-16 11:34:31 +0200

    heroicons

--- resources/js/Components/Chat/ConversationItem.vue
+++ resources/js/Components/Chat/ConversationItem.vue
@@ -1,31 +1,34 @@
 <template>
-  <Link href="#" class="flex items-center gap-2 p-2 hover:bg-slate-400/30 transition-all">
-    <!-- Avatar de profil pour les users simples -->
-    <UserAvatar
-      :avatar="conversation.avatar"
-      :name="conversation.name"
-      :isOnline="isOnline"
-      v-if="conversation.is_user"
-    />
-    <!-- Avatar de profile pour les groupes -->
-    <GroupAvatar v-if="conversation.is_group" />
-    <div class="flex-1 max-w-full overflow-hidden text-xs">
-      <div class="flex gap-1 justify-between items-center">
-        <h3 class="font-semibold text-sm text-nowrap text-ellipsis truncate">
-          {{ conversation.name }}
-        </h3>
-        <span v-if="conversation.last_message_date" class="text-nowrap italic">
-          {{ conversation.last_message_date }}
-        </span>
+  <div class="flex ">
+    <Link href="#" class="flex items-center gap-2 p-2 hover:bg-slate-400/30 transition-all w-full ">
+      <!-- Avatar de profil pour les users simples -->
+      <UserAvatar
+        :avatar="conversation.avatar"
+        :name="conversation.name"
+        :isOnline="isOnline"
+        v-if="conversation.is_user"
+      />
+      <!-- Avatar de profile pour les groupes -->
+      <GroupAvatar v-if="conversation.is_group" />
+      <div class="flex-1 max-w-full overflow-hidden text-xs">
+        <div class="flex gap-1 justify-between items-center">
+          <h3 class="font-semibold text-sm text-nowrap text-ellipsis truncate">
+            {{ conversation.name }}
+          </h3>
+          <span v-if="conversation.last_message_date" class="text-nowrap italic">
+            {{ conversation.last_message_date }}
+          </span>
+        </div>
+        <p
+          v-if="conversation.last_message"
+          class="text-xs text-nowrap overflow-hidden text-ellipsis truncate"
+        >
+          {{ conversation.last_message }}
+        </p>
       </div>
-      <p
-        v-if="conversation.last_message"
-        class="text-xs text-nowrap overflow-hidden text-ellipsis truncate"
-      >
-        {{ conversation.last_message }}
-      </p>
-    </div>
-  </Link>
+    </Link>
+    <UserOptionsDropdown class="inline-block cursor-pointer basis-1" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -33,6 +36,7 @@ import { Conversation } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
 import UserAvatar from './UserAvatar.vue';
 import GroupAvatar from './GroupAvatar.vue';
+import UserOptionsDropdown from '../UserOptionsDropdown.vue';
 
 const page = usePage();
 
---
+++ resources/js/Components/UserOptionsDropdown.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Menu as="div" class="relative inline-block text-left" >
+            <MenuButton class="inline-flex w-full justify-center rounded-md " >
+                <span class="sr-only" >Ouvrir le menu contextuel</span>
+
+            </MenuButton>
+            <Transition enter-active-class="" >
+                <MenuItems class="absolute right-0 mt-2 w-56 origin-top-right divide-y" >
+                    <MenuTiem>Menu 1</MenuTiem>
+                    <MenuTiem>Menu 2</MenuTiem>
+                    <MenuTiem></MenuTiem>
+                
+                </MenuItems>
+
+            </Transition>
+
+        </Menu>
+        <Icon icon="heroicons:ellipsis-vertical" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+import { Icon } from '@iconify/vue';
+</script>
+
+<style scoped></style>
